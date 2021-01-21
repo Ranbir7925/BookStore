51346d9661589122ee23a07f5beb9dfe4e7ab1bb
Ranbir Singh <honeyranbir7925@gmail.com>
2021-01-21 18:30:49 +0530

    add:Book component to get all the data of books avaliable

--- src/components/book/Books.vue
+++ src/components/book/Books.vue
@@ -1,38 +1,44 @@
 <template>
-  <div>
-    <Sort v-bind:noOfBooks="noOfBooks"/>
+  <v-app>
+    <div class="outter">
+      <div class="books">
+        <div class="book-count">Books</div>
+        <span>({{ noOfBooks + ` items` }})</span>
+      </div>
+      <div class="dropdown">
+        <v-select
+          :items="abc"
+          v-model="selected"
+          @change="sorting"
+          label="Sort by relevence"
+          dense
+          solo
+        ></v-select>
+      </div>
+    </div>
     <DisplayBooks v-bind:bookList="bookList" v-bind:noOfBooks="noOfBooks" />
-    <md-button class="md-raised" @click="sorting">wishlist</md-button>
-  </div>
+  </v-app>
 </template>
 <script>
 import DisplayBooks from "./DisplayBooks";
-import Sort from "./Sort";
 import BookService from "../../service/bookService";
 export default {
-  components: { DisplayBooks ,Sort},
+  components: { DisplayBooks },
   data() {
     return {
       bookList: [],
       noOfBooks: "",
+      selected: "",
+      abc: ["Price: Low to High", "Price: High to Low", "Newest Arrivals"],
     };
   },
   methods: {
-    // fetchBooksList: function () {
-    //   BookService.fetchBookList()
-    //     .then((response) => {
-    //       console.log(response.data.result);
-    //       this.bookList = response.data.result;
-    //     })
-    //     .catch((error) => console.log(error));
-    // },
-
     fetchBooksList: function () {
       BookService.fetchBookList()
         .then((response) => {
           console.log(response.data.result);
           this.bookList = response.data.result;
-          this.bookList.map(function (item,index) {
+          this.bookList.map(function (item, index) {
             item.bookImage = `book${index}`;
           });
           this.noOfBooks = this.bookList.length;
@@ -45,14 +51,25 @@ export default {
       return function (a, b) {
         if (a[property] > b[property]) return 1;
         else if (a[property] < b[property]) return -1;
-
         return 0;
       };
     },
 
     sorting: function () {
-      this.bookList.sort(this.sortByProperty("price"));
-      console.log("sorted", this.bookList);
+      if (this.selected === "Price: Low to High") {
+        this.bookList.sort(this.sortByProperty("price"));
+        console.log("sorted", this.bookList);
+      }
+      if (this.selected === "Price: High to Low") {
+        let temp = [];
+        temp = this.bookList.sort(this.sortByProperty("price"));
+        this.bookList = temp.reverse();
+        console.log("sorted", this.bookList);
+      }
+      if (this.selected === "Newest Arrivals") {
+        this.bookList.reverse();
+        console.log("na", this.bookList);
+      }
     },
   },
   created() {
@@ -60,3 +77,33 @@ export default {
   },
 };
 </script>
+
+<style scoped>
+.outter {
+  padding-left: 9%;
+  padding-right: 12%;
+  display: flex;
+  justify-content: space-between;
+  align-items: center;
+  flex-direction: row;
+  height: 60px;
+}
+span {
+  opacity: 0.5;
+  height: 15px;
+  padding-left: 4px;
+}
+
+.books {
+  display: flex;
+  flex-direction: row;
+  align-items: center;
+}
+.book-count {
+  font-size: xx-large;
+}
+.dropdown {
+  height: 35px;
+  width: 200px;
+}
+</style>
