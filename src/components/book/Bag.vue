d352872577869f5ef256e225dd2804ed6060b634
Ranbir Singh <honeyranbir7925@gmail.com>
2021-01-21 18:32:56 +0530

    refactor:component to perform add to cart action

---
+++ src/components/book/Bag.vue
@@ -0,0 +1,38 @@
+<template>
+  <div>
+    <md-button class="md-raised md-primary right" @click="addItemToCart"
+      >add to bag</md-button
+    >
+  </div>
+</template>
+<script>
+import { eventBus } from "../../main";
+import bookService from "../../service/bookService";
+export default {
+  name: "Bag",
+  props: ["bookId"],
+  data() {
+    return {};
+  },
+  methods: {
+    addItemToCart: function () {
+      bookService
+        .addToCart(this.bookId)
+        .then((response) => {
+          console.log(response);
+          eventBus.$emit("addToCart");
+        })
+        .catch((err) => {
+          console.log(err);
+        });
+    },
+  },
+};
+</script>
+<style >
+.right {
+  background-color: #a03037 !important;
+  box-shadow: none !important;
+    border: 1px solid #BFBFBF;
+}
+</style>
