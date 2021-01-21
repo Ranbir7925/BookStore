4f7047f49a1d0019a22443a6406be4cf3a18cd8d
Ranbir Singh <honeyranbir7925@gmail.com>
2021-01-21 18:33:24 +0530

    refactor:component to perform add to wishlist action

---
+++ src/components/book/Wishlist.vue
@@ -0,0 +1,39 @@
+<template>
+  <div>
+    <md-button class="md-raised button" @click="addItemToWishlist">wishlist</md-button>
+  </div>
+</template>
+<script>
+import bookService from '../../service/bookService'
+import { eventBus } from '../../main'
+export default {
+    name:"Wishlist",
+    props:["bookId"],
+    data(){
+        return{
+
+        }
+    },
+    methods:{
+            addItemToWishlist: function () {
+      bookService
+        .addToWishlist(this.bookId)
+        .then((response) => {
+          console.log(response);
+          eventBus.$emit("addToCart");
+        })
+        .catch((err) => {
+          console.log(err);
+        });
+    },
+    }
+
+}
+</script>
+
+<style >
+    .button{
+      box-shadow: none !important;
+    border: 1px solid #BFBFBF;
+    }
+</style>
